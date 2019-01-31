b54dbda6d2b5e0249e4b2757f8570789795847bb
Giedrius <giedrius@blokas.io>
2019-02-01 01:08:47 +0200

    Use patch username (password: patch) Change the hostname to patchbox

--- stage3/01-enable-ssh/00-run.sh
+++ stage3/01-enable-ssh/00-run.sh
@@ -1,2 +1,7 @@
 # Enable ssh.
 touch ${ROOTFS_DIR}/boot/ssh
+
+# Set the new hostname.
+CURRENT_HOSTNAME=`cat ${ROOTFS_DIR}/etc/hostname | tr -d " \t\n\r"`
+echo $HOSTNAME > ${ROOTFS_DIR}/etc/hostname
+sed -i 's/127.0.1.1.*'"$CURRENT_HOSTNAME"'/127.0.1.1\t'"$HOSTNAME"'/g' ${ROOTFS_DIR}/etc/hosts
