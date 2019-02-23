4d670bf9380e4fa078ace13dd7ceca846228a84b
Giedrius <giedrius@blokas.io>
2019-02-23 15:49:25 +0200

    Added Pianoteq, Sonic Pi, various tweaks

--- stage3/04-install-sw/01-run.sh
+++ stage3/04-install-sw/01-run.sh
@@ -10,5 +10,13 @@ cp files/.pdsettings ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.pdsettings
 mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.audacity-data
 cp files/audacity.cfg ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.audacity-data/
 
+# Install desktop shortcuts.
 mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop
 cp files/*.desktop ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/
+
+# For unknown reason SuperColliderIDE.desktop does not show up in system menu, unless it's renamed...
+mv ${ROOTFS_DIR}/usr/share/applications/SuperColliderIDE.desktop ${ROOTFS_DIR}/usr/share/applications/SuperCollider.desktop
+
+# Copy preconfigured Pianoteq settings.
+mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/Modartt
+cp files/Pianoteq64.prefs ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/Modartt/
