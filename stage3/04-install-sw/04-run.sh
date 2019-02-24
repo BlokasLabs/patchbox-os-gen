fe933f22cc68bb3a87c68aec1d23a97553b538df
Giedrius <giedrius@blokas.io>
2019-02-24 18:23:08 +0200

    Various system settings for 'pi' user ported for 'patch', touchosc2midi service, desktop shortcuts, etc...

--- stage3/04-install-sw/04-run.sh
+++ stage3/04-install-sw/04-run.sh
@@ -1,2 +1,5 @@
 # Allow 'startx' to be used over SSH.
 sed -i 's/allowed_users=console/allowed_users=anybody/g' ${ROOTFS_DIR}/etc/X11/Xwrapper.config
+
+# Fix up policy for 'patch' user.
+install -m 644 files/60-desktop-policy.conf ${ROOTFS_DIR}/etc/polkit-1/localauthority.conf.d/
