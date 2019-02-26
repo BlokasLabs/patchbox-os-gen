17058721fff6a2bedefcd1c57e0cda76344c9e83
Giedrius <giedrius@blokas.io>
2019-02-26 19:35:13 +0200

    Small fixes

--- stage3/04-install-sw/08-run.sh
+++ stage3/04-install-sw/08-run.sh
@@ -1,6 +1,8 @@
 # Set up graphical environment appearance.
 cp files/blokas-logo.png ${ROOTFS_DIR}/usr/share/images/
+mkdir -p ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi
 cp files/desktop-items-0.conf ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/
+cp files/pi-greeter.conf ${ROOTFS_DIR}/etc/lightdm/
 
 # Set up MOTD
 cp files/motd ${ROOTFS_DIR}/etc/motd
