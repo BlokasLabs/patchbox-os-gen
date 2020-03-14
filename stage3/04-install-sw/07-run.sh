a2cdfd4581e8a5647c057b62e038532f0373ccfe
Giedrius <giedrius@blokas.io>
2020-03-14 17:45:50 +0200

    GUI customizations and various fixes

--- stage3/04-install-sw/07-run.sh
+++ stage3/04-install-sw/07-run.sh
@@ -1,14 +1,2 @@
-if [ ! -d ${ROOTFS_DIR}/usr/local/patchbox-cli ]; then
-	echo "Cloning patchbox-cli repository from https://github.com/BlokasLabs/patchbox-cli..."
-	git clone https://github.com/BlokasLabs/patchbox-cli.git ${ROOTFS_DIR}/usr/local/patchbox-cli
-else
-	echo "Updating patchbox-cli repository with latest stuff in https://github.com/BlokasLabs/patchbox-cli..."
-	cd ${ROOTFS_DIR}/usr/local/patchbox-cli && git -c user.name="apt-get" -c user.email="apt@get" stash && git pull
-fi
-
-on_chroot << EOF
-	cd /usr/local/patchbox-cli && sudo pip install --editable .
-EOF
-
 cp -p files/first-run.sh ${ROOTFS_DIR}/etc/profile.d/
 cd ${ROOTFS_DIR}/etc/profile.d/ && rm -f sshpwd.sh wifi-country.sh
--- stage3/04-install-sw/08-run.sh
+++ stage3/04-install-sw/08-run.sh
@@ -1,8 +1,9 @@
 # Set up graphical environment appearance.
-cp files/blokas-logo.png ${ROOTFS_DIR}/usr/share/images/
+cp files/blokas-logo.png files/blokas-logo.svg ${ROOTFS_DIR}/usr/share/images/
 mkdir -p ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi
 cp files/desktop-items-0.conf ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/
 cp files/pi-greeter.conf ${ROOTFS_DIR}/etc/lightdm/
+cp files/lxde-panel ${ROOTFS_DIR}/home/patch/.config/lxpanel/LXDE-pi/panels/panel
 
 # Set up MOTD
 cp files/motd ${ROOTFS_DIR}/etc/motd
