647dc724c2dc1bc788ebf550b3a04f77324871b7
Giedrius <giedrius@blokas.io>
2020-03-15 00:17:16 +0200

    Remove motd as it comes from patchbox-cli, fixed home button customization

--- stage3/04-install-sw/08-run.sh
+++ stage3/04-install-sw/08-run.sh
@@ -1,9 +1,6 @@
 # Set up graphical environment appearance.
 cp files/blokas-logo.png files/blokas-logo.svg ${ROOTFS_DIR}/usr/share/images/
-mkdir -p ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi
+mkdir -p ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/panels
 cp files/desktop-items-0.conf ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/
 cp files/pi-greeter.conf ${ROOTFS_DIR}/etc/lightdm/
 cp files/lxde-panel ${ROOTFS_DIR}/home/patch/.config/lxpanel/LXDE-pi/panels/panel
-
-# Set up MOTD
-cp files/motd ${ROOTFS_DIR}/etc/motd
