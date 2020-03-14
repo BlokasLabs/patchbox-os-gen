07451ffbbece5f58d748c703a39f66d33b88a094
Giedrius <giedrius@blokas.io>
2020-03-15 01:39:15 +0200

    Set 1280x720 resolution by default, removed Sonic Pi icon from desktop, will be available as a module once 3.2.0 deb is ready

--- stage3/04-install-sw/08-run.sh
+++ stage3/04-install-sw/08-run.sh
@@ -4,3 +4,8 @@ mkdir -p ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/panels
 cp files/desktop-items-0.conf ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/
 cp files/pi-greeter.conf ${ROOTFS_DIR}/etc/lightdm/
 cp files/lxde-panel ${ROOTFS_DIR}/home/patch/.config/lxpanel/LXDE-pi/panels/panel
+
+# Configure 1280x720 screen size by default.
+sed -i -E "s/#?hdmi_group=[0-9]+/hdmi_group=2/" ${ROOTFS_DIR}/boot/config.txt
+sed -i -E "s/#?hdmi_mode=[0-9]+/hdmi_mode=85/" ${ROOTFS_DIR}/boot/config.txt
+sed -i -E "s/#?hdmi_force_hotplug=[0-9]+/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/config.txt
