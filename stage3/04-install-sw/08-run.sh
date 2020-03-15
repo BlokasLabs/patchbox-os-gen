43ebb99244de001cfafcb6d62f0ff3de952cb3e1
Giedrius <giedrius@blokas.io>
2020-03-15 03:23:40 +0200

    Fixed LXDE panels setup

--- stage3/04-install-sw/08-run.sh
+++ stage3/04-install-sw/08-run.sh
@@ -1,8 +1,9 @@
 # Set up graphical environment appearance.
 cp files/blokas-logo.png files/blokas-logo.svg ${ROOTFS_DIR}/usr/share/images/
-mkdir -p ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/panels
+mkdir -p ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/
 cp files/desktop-items-0.conf ${ROOTFS_DIR}/home/patch/.config/pcmanfm/LXDE-pi/
 cp files/pi-greeter.conf ${ROOTFS_DIR}/etc/lightdm/
+mkdir -p ${ROOTFS_DIR}/home/patch/.config/lxpanel/LXDE-pi/panels
 cp files/lxde-panel ${ROOTFS_DIR}/home/patch/.config/lxpanel/LXDE-pi/panels/panel
 
 # Configure 1280x720 screen size by default.
