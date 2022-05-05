65361b0fd936aacf5f96e0ce05ef855000df99bc
Giedrius <giedrius@blokas.io>
2022-05-05 15:01:49 +0300

    Use signed-by for Blokas APT server

--- stage3/00-configure-apt/00-run.sh
+++ stage3/00-configure-apt/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 install -m 644 files/blokas.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
+install -m 644 files/blokas-archive-keyring.gpg ${ROOTFS_DIR}/usr/share/keyrings/
 
-on_chroot apt-key add - < files/blokas.gpg.key
 on_chroot << EOF
 apt-get update
 EOF
