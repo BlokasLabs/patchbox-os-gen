b28ed14bb211096d6651123adbdfe4b0d67d9d5d
Giedrius <giedrius@blokas.io>
2022-05-14 15:35:52 +0300

    Update motd

--- stage3/04-install-sw/files/patchbox-first-run.sh
+++ stage3/04-install-sw/files/patchbox-first-run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 if [ ! -e ~/.config/patchbox-wizard-run ]; then
 	touch ~/.config/patchbox-wizard-run
+	sudo chmod -x /etc/update-motd.d/21-patchbox-useful-resources
 	sudo patchbox-config wizard
+	clear && cat /run/motd.dynamic
 fi
--- stage3/99-fixup-perm/00-run.sh
+++ stage3/99-fixup-perm/00-run.sh
@@ -3,4 +3,6 @@ on_chroot << EOF
 	chgrp -R ${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
 
 	chmod -R 777 /usr/local/puredata-patches
+
+	apt-get autoremove -y
 EOF
